187979b88aaf268f36ae93cf17c633db91b22d0a
oualaeddine <berrehaloualaeddine@gmail.com>
2016-10-06 11:09:39 +0200

    now it's working :P

---
+++ app/src/main/java/dz/da3sou9a/oualaeddine/lmdulator/activities/frags/ModulesListView.java
@@ -0,0 +1,15 @@
+package dz.da3sou9a.oualaeddine.lmdulator.activities.frags;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dz.da3sou9a.oualaeddine.lmdulator.R;
+
+public class ModulesListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_modules_list_view_container);
+    }
+}
